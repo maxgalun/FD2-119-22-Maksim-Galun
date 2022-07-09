8337cc80229755fe017ebb1880877ae2d9beadf1
Maksim Galun <maxgalun@gmail.com>
2022-07-10 02:51:54 +0300

    On branch ANKETA Changes to be committed: 	new file:   ANKETA/index.html 	new file:   ANKETA/index.js 	deleted:    HOME/LESSON1/index.html

---
+++ ANKETA/index.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const malePensionAge = 63;
+const femalePensionAge = 58;
+const ageLimit = 120;
+
+function isEmptyStr(str) {
+   return !str?.trim();
+}
+
+function checkStr(str) {
+   return str.trim()[0].toUpperCase() + str.trim().substring(1).toLowerCase();
+}
+
+function getFIO(propStr) {
+   let prop;
+   for (;;) {
+      prop = prompt(`Укажите ${propStr}`);
+      if (!isEmptyStr(prop)) break;
+   }
+   return checkStr(prop);
+}
+
+function getAge() {
+   let age;
+   for (;;) {
+      age = prompt("Укажите возраст");
+      if (
+         !isEmptyStr(age) &&
+         !isNaN(age) &&
+         Number.isInteger(+age) &&
+         +age < ageLimit
+      )
+         break;
+   }
+   return +age;
+}
+
+function isPensioner(isMale, age) {
+   return (isMale && age >= malePensionAge) ||
+      (!isMale && age >= femalePensionAge)
+      ? true
+      : false;
+}
+
+const surname = getFIO("фамилию");
+const name = getFIO("имя");
+const patronymic = getFIO("отчество");
+const age = getAge();
+const isMale = confirm("Ваш пол - мужской?");
+
+alert(`ваше ФИО: ${surname} ${name} ${patronymic}
+ваш возраст в годах: ${age}
+ваш возраст в днях: ${age * 365}
+через 5 лет вам будет: ${age + 5}
+ваш пол: ${isMale ? "мужской" : "женский"}
+вы на пенсии: ${isPensioner() ? "да" : "нет"}`);

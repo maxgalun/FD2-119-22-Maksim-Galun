1cde59eb651a69040a492e56328dfcfaaec19f83
Maksim Galun <maxgalun@gmail.com>
2022-07-10 23:19:14 +0300

    Changes to be committed: 	modified:   TREESUM/index.js

--- TREESUM/index.js
+++ TREESUM/index.js
@@ -1,1 +1,13 @@
-function treeSum(array) {}
+const arr = [5, 7, [4, [2], 8, [1, 3], 2], [9, []], 1, 8];
+
+function treeSum(array) {
+   let result = 0;
+   for (let elem = 0; elem < array.length; elem++) {
+      Array.isArray(array[elem])
+         ? (result += treeSum(array[elem]))
+         : (result += array[elem]);
+   }
+   return result;
+}
+
+console.log(treeSum(arr));

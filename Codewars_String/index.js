5fc5e19a091e7398ac3b89086bf46ad8f702630a
Maksim Galun <maxgalun@gmail.com>
2022-07-12 21:17:52 +0300

    Changes to be committed: 	modified:   Codewars_String/index.js

--- Codewars_String/index.js
+++ Codewars_String/index.js
@@ -13,7 +13,10 @@ console.log(
 );
 console.log(disemvowel("What are you, a communist?"));
 
-/*-----------------------------------------------------------------------------------*/
+console.log(
+   "-----------------------------------------------------------------------------------------------------"
+);
+/*-------------------------------------------------------------------------------------------------------------------*/
 
 function accum(s) {
    let result = "";
@@ -31,7 +34,10 @@ console.log(accum("MjtkuBovqrU"));
 console.log(accum("EvidjUnokmM"));
 console.log(accum("HbideVbxncC"));
 
-/*-----------------------------------------------------------------------------------*/
+console.log(
+   "-----------------------------------------------------------------------------------------------------"
+);
+/*-------------------------------------------------------------------------------------------------------------------*/
 
 function highAndLow(numbers) {
    numbers = numbers.split(" ").sort((a, b) => b - a);
@@ -41,7 +47,10 @@ function highAndLow(numbers) {
 console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
 console.log(highAndLow("1 2 3"));
 
-/*-----------------------------------------------------------------------------------*/
+console.log(
+   "-----------------------------------------------------------------------------------------------------"
+);
+/*-------------------------------------------------------------------------------------------------------------------*/
 
 function isIsogram(str) {
    for (let i = 0; i < str.length; i++) {
@@ -59,12 +68,71 @@ console.log(isIsogram("moOse"));
 console.log(isIsogram("isIsogram"));
 console.log(isIsogram(""));
 
-/*-----------------------------------------------------------------------------------*/
+console.log(
+   "-----------------------------------------------------------------------------------------------------"
+);
+/*-------------------------------------------------------------------------------------------------------------------*/
 
-function calc(x) {}
+function calc(x) {
+   let total1 = 0;
+   let total2 = 0;
+
+   for (let i = 0; i < x.length; i++) {
+      x[i]
+         .charCodeAt()
+         .toString()
+         .split("")
+         .forEach((element) => {
+            total1 += +element;
+            if (element == "7") element = 1;
+            total2 += +element;
+         });
+   }
+
+   return total1 - total2;
+}
 
 console.log(calc("abcdef"));
 console.log(calc("ifkhchlhfd"));
 console.log(calc("aaaaaddddr"));
 console.log(calc("jfmgklf8hglbe"));
 console.log(calc("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+
+console.log(
+   "-----------------------------------------------------------------------------------------------------"
+);
+/*-------------------------------------------------------------------------------------------------------------------*/
+
+function catMouse(map, moves) {
+   if (!(map.includes("C") && map.includes("m"))) {
+      return "boring without two animals";
+   }
+
+   let positionCat;
+   let positionMouse;
+
+   map = map.split("\n");
+
+   for (let i = 0; i < map.length; i++) {
+      if (map[i].includes("C")) positionCat = i + map[i].indexOf("C");
+      if (map[i].includes("m")) positionMouse = i + map[i].indexOf("m");
+   }
+
+   return positionMouse - positionCat < moves ? "Caught!" : "Escaped!";
+}
+
+var map1 = `..C......
+.........
+....m....`;
+
+var map2 = `.C.......
+.........
+......m..`;
+
+var map3 = `..C......
+.........
+.........`;
+
+console.log(catMouse(map1, 5));
+console.log(catMouse(map2, 5));
+console.log(catMouse(map3, 5));

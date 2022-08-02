68d3ce93537351bfaed82a694969ffa5c4776ec7
Maksim Galun <maxgalun@gmail.com>
2022-08-03 00:37:18 +0300

    Changes to be committed: 	modified:   A4+/index.js

--- A4+/index.js
+++ A4+/index.js
@@ -1,17 +1,26 @@
 "use strict";
 
-function format(str) {
-   return str
+// function format(str) {
+//    return str
+//       .toLowerCase()
+//       .replaceAll(/[—.,;:?!-"()\s]/g, "")
+//       .replaceAll("ё", "е")
+//       .replaceAll("ъ", "ь");
+// }
+
+function palindrome(str) {
+   str = str
       .toLowerCase()
       .replaceAll(/[—.,;:?!-"()\s]/g, "")
       .replaceAll("ё", "е")
       .replaceAll("ъ", "ь");
-}
 
-function palindrome(str) {
-   if (str.length < 2) return true;
-
-   return str[0] == str[str.length - 1] ? palindrome(str.slice(1, -1)) : false;
+   return (function recursion(str) {
+      if (str.length < 2) return true;
+      return str[0] == str[str.length - 1]
+         ? recursion(str.slice(1, -1))
+         : false;
+   })(str);
 }
 
 let str1 = "А роза упала на лапу Азора";
@@ -20,22 +29,8 @@ let str3 = "Я иду съ мечемъ судия";
 let str4 = "Я — арка края";
 let str5 = "Саша на полке клопа нашёл";
 
-console.log(
-   `${str1} - ${palindrome(format(str1)) ? "палиндром" : "не палиндром"}`
-);
-
-console.log(
-   `${str2} - ${palindrome(format(str2)) ? "палиндром" : "не палиндром"}`
-);
-
-console.log(
-   `${str3} - ${palindrome(format(str3)) ? "палиндром" : "не палиндром"}`
-);
-
-console.log(
-   `${str4} - ${palindrome(format(str4)) ? "палиндром" : "не палиндром"}`
-);
-
-console.log(
-   `${str5} - ${palindrome(format(str5)) ? "палиндром" : "не палиндром"}`
-);
+console.log(`${str1} - ${palindrome(str1) ? "палиндром" : "не палиндром"}`);
+console.log(`${str2} - ${palindrome(str2) ? "палиндром" : "не палиндром"}`);
+console.log(`${str3} - ${palindrome(str3) ? "палиндром" : "не палиндром"}`);
+console.log(`${str4} - ${palindrome(str4) ? "палиндром" : "не палиндром"}`);
+console.log(`${str5} - ${palindrome(str5) ? "палиндром" : "не палиндром"}`);

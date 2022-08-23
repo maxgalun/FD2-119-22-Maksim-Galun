406ac5b387cfb898a55877653fb0fb58653a6ef6
Maksim Galun <maxgalun@gmail.com>
2022-08-23 19:04:10 +0300

    Changes to be committed: 	modified:   index.html 	modified:   index.js

--- DRINKS_HASH_FUNC/index.js
+++ DRINKS_HASH_FUNC/index.js
@@ -1,18 +1,19 @@
 function HashStorageFunc() {
-   this.hash = {};
+   const hash = {};
 
    this.addValue = function (key, value) {
-      this.hash[key] = value;
+      hash[key] = value;
    };
    this.getValue = function (key) {
-      return this.hash[key];
+      return hash[key];
    };
    this.deleteValue = function (key) {
-      return delete this.hash[key];
+      if (key in hash) return delete hash[key];
+      return false;
    };
 
    this.getKeys = function () {
-      return Object.keys(this.hash);
+      return Object.keys(hash);
    };
 }
 
@@ -20,18 +21,40 @@ const drinkStorage = new HashStorageFunc();
 
 function addDrink() {
    const name = prompt("Укажите название напитка");
-   const alco = confirm("Напиток алкогольный?");
+   const isAlco = confirm("Напиток алкогольный?");
    const recipe = prompt("Укажите рецепт приготовления");
    drinkStorage.addValue(name, {
-      alco,
+      isAlco,
       recipe,
    });
+
+   console.log(`Данные о напитке ${name} добавлены`);
 }
 
-function getDrink(key) {
-   // const alco = this.hash[key]["alco"];
-   // const recipe = this.hash[key]["recipe"];
-   // console.log(`напиток: ${key}
-   // алкогольный: ${alco ? "да" : "нет"}
-   // рецепт приготовления: ${recipe}`);
+function getDrink(name) {
+   if (!drinkStorage.getValue(name)) {
+      console.log(`Нет информации о напитке ${name}`);
+      return;
+   }
+
+   const isAlco = drinkStorage.getValue(name)["isAlco"];
+   const recipe = drinkStorage.getValue(name)["recipe"];
+
+   console.log(`напиток: ${name}
+алкогольный: ${isAlco ? "да" : "нет"}
+рецепт приготовления: ${recipe}`);
+}
+
+function deleteDrink(name) {
+   console.log(
+      drinkStorage.deleteValue(name)
+         ? `Информация о напитке ${name} удалена`
+         : "Такого напитка нет"
+   );
+}
+
+function getDrinksName() {
+   for (let name of drinkStorage.getKeys()) {
+      console.log(name);
+   }
 }

ac0537fe7155cc1fe75cd3f2098ea6c78229bfac
Maksim Galun <maxgalun@gmail.com>
2022-08-26 17:05:13 +0300

    Changes to be committed: 	modified:   index.html 	modified:   index.js 	modified:   style.css

--- CREATE_TAGS/index.js
+++ CREATE_TAGS/index.js
@@ -1,12 +1,12 @@
-class Form {
-  constructor(form) {
-    this._form = form;
-    form.onclick = this.onClick.bind(this);
-  }
+class Menu {
+   constructor(form) {
+      this._form = form;
+      form.onclick = this.onClick.bind(this);
+   }
 
-  createPopup(tag) {
-    document.getElementById("modal").style.display = "block";
-    const popupHTML = `<div class="modal__popup popup">
+   createPopup(tag) {
+      document.getElementById("modal").style.display = "block";
+      const popupHTML = `<div class="modal__popup popup">
    <div class="popup__content">
       <div class="popup__header">
          <div class="popup__title">Укажите содержимое для элемента ${tag}</div>
@@ -15,9 +15,9 @@ class Form {
       <input type="text" class="popup__input" placeholder='enter content'>
       </div>
       ${
-        tag == "ul"
-          ? '<button id="plus" class="popup__plus" data-action="clone">+</button>'
-          : ""
+         tag == "ul"
+            ? '<button id="plus" class="popup__plus" data-action="clone">+</button>'
+            : ""
       }
       <div id="popup__footer" class="popup__footer">
          <button class="popup__button" data-action="add">
@@ -30,70 +30,72 @@ class Form {
    </div>
 </div>`;
 
-    document.getElementById("modal").innerHTML = popupHTML;
-  }
+      document.getElementById("modal").innerHTML = popupHTML;
+   }
 
-  cancel() {
-    document.getElementsByClassName("popup")[0].remove();
-    document.getElementById("modal").style.display = "none";
-  }
+   cancel() {
+      document.getElementsByClassName("popup")[0].remove();
+      document.getElementById("modal").style.display = "none";
+   }
 
-  add(tag) {
-    let childNode;
-    switch (tag) {
-      case "ul":
-        const ul = document.createElement("ul");
+   add(tag) {
+      let childNode;
+      switch (tag) {
+         case "ul":
+            const ul = document.createElement("ul");
 
-        [...document.getElementsByClassName("popup__input")].forEach(
-          (element) => {
-            if (element.value) {
-              const textNode = document.createTextNode(element.value);
-              const li = document.createElement("li");
-              li.appendChild(textNode);
-              ul.appendChild(li);
-            }
-          }
-        );
-        childNode = ul;
+            [...document.getElementsByClassName("popup__input")].forEach(
+               (element) => {
+                  if (element.value) {
+                     const textNode = document.createTextNode(element.value);
+                     const li = document.createElement("li");
+                     li.appendChild(textNode);
+                     ul.appendChild(li);
+                  }
+               }
+            );
+            childNode = ul;
 
-        break;
+            break;
 
-      default:
-        const textNode = document.createTextNode(
-          document.getElementsByClassName("popup__input")[0].value
-        );
-        const tagNode = document.createElement(tag);
-        tagNode.appendChild(textNode);
-        childNode = tagNode;
-        break;
-    }
+         default:
+            const textNode = document.createTextNode(
+               document.getElementsByClassName("popup__input")[0].value
+            );
+            const tagNode = document.createElement(tag);
+            tagNode.appendChild(textNode);
+            childNode = tagNode;
+            break;
+      }
 
-    document.getElementById("app").appendChild(childNode);
+      document.getElementById("app").appendChild(childNode);
 
-    this.cancel();
-  }
+      this.cancel();
+   }
 
-  clone() {
-    const inputElement = document
-      .getElementsByClassName("popup__input")[0]
-      .cloneNode();
-    inputElement.value = "";
-    document.getElementsByClassName("popup__body")[0].appendChild(inputElement);
-  }
+   clone() {
+      const inputElement = document
+         .getElementsByClassName("popup__input")[0]
+         .cloneNode();
+      inputElement.value = "";
+      document
+         .getElementsByClassName("popup__body")[0]
+         .appendChild(inputElement);
+   }
 
-  onClick(event) {
-    const tag = event.target.dataset.tag;
-    const action = event.target.dataset.action;
+   onClick(event) {
+      const tag = event.target.dataset.tag;
+      const action = event.target.dataset.action;
 
-    if (tag) {
-      this.tag = tag;
-      this.createPopup(tag);
-    }
+      if (tag) {
+         this.tag = tag;
+         this.createPopup(tag);
+      }
 
-    if (action) {
-      this[action](this.tag);
-    }
-  }
+      if (action) {
+         this[action](this.tag);
+      }
+   }
 }
 
-new Form(document.forms.form1);
+new Menu(document.getElementById("menu"));
